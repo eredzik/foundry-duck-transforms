5fafcf5dbb469a2a55df24cc5c8ed6fab65ae479
Emil Redzik <emilredzik1994@gmail.com>
2024-10-14 14:58:39 +0200

    wip

--- transforms/api/__init__.py
+++ transforms/api/__init__.py
@@ -1,3 +1,4 @@
+from .check import Check
 from .transform_df import Input, Output, Transform, transform_df
 
 __all__= [
@@ -5,4 +6,5 @@ __all__= [
     "Output",
     "Transform",
     "transform_df",
+    "Check"
 ]
