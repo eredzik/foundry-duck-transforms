e78b8c1d53e300fb9bf2b78f6fa4ee2167e2d7f4
Emil Redzik <emilredzik1994@gmail.com>
2024-10-14 15:01:27 +0200

    wip

--- transforms/api/transform_df.py
+++ transforms/api/transform_df.py
@@ -9,20 +9,29 @@ from .check import Check
 @dataclass
 class Input:
     path_or_rid: str
+    checks: Check | None | list[Check] = None
+
 
 @dataclass
 class Output:
-    path_or_rid:str
-    checks: Check | None = None
+    path_or_rid: str
+    checks: Check | None | list[Check] = None
+
 
 TransformParamSpec = ParamSpec("TransformParamSpec")
+
+
 @dataclass
 class Transform(Generic[TransformParamSpec]):
-    inputs: dict[str,Input]
+    inputs: dict[str, Input]
     outputs: list[Output]
     transform: Callable[TransformParamSpec, DataFrame]
 
-DecoratorParamSpec = ParamSpec("DecoratorParamSpec", )
+
+DecoratorParamSpec = ParamSpec(
+    "DecoratorParamSpec",
+)
+
 
 def transform_df(output: Output, **kwargs: Input):
     TParams = ParamSpec("TParams")
@@ -31,9 +40,10 @@ def transform_df(output: Output, **kwargs: Input):
     def _transform_df(transform: Callable[Concatenate[TParams], DataFrame]):
         # TODO: Add verification if all params are correctly specified
         return Transform(inputs=kwargs, outputs=[output], transform=transform)
+
     return _transform_df
+
 
 @transform_df(output=Output("dataset"), df1=Input("dataset"), df2=Input("dataset"))
 def transformed(df1: DataFrame, df2: DataFrame):
     return df1
-
