d9dd4458ff8138232dbe19b8bebca5e91515dfdd
Emil Redzik <emilredzik1994@gmail.com>
2024-11-01 16:23:09 +0100

    added check info print

--- transforms/runner/exec_check.py
+++ transforms/runner/exec_check.py
@@ -6,6 +6,7 @@ from ..api.check import Check
 def execute_check(df: DataFrame, check: Check ):
     try:
         check.expectation.run(df)
+        print("Check {check.description} passed successfully")
     except Exception as e:
         print(f"{check.on_error}: Check {check.description} failed with error {e}")
         if check.on_error=='FAIL':
