f2222da78fff9e40040260b7906d5d87625e26a4
Emil Redzik <emilredzik1994@gmail.com>
2024-11-01 15:43:24 +0100

    moved to appropriate location

--- transforms/api/__init__.py
+++ transforms/api/__init__.py
@@ -3,7 +3,7 @@ from .configure import configure
 from .incremental_transform import incremental
 from .transform_df import Input, Output, Transform, transform, transform_df
 
-__all__= [
+__all__ = [
     "Input",
     "Output",
     "Transform",
