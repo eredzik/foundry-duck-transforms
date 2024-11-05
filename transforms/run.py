be54a8c1ae61cdc1805d51339c3d8643f519c8fb
Emil Redzik <emilredzik1994@gmail.com>
2024-11-05 13:15:31 +0100

    fix: updated imports order

--- transforms/run.py
+++ transforms/run.py
@@ -1,18 +1,9 @@
-import importlib.util
 import sys
 from enum import Enum
-from hashlib import sha256
 from pathlib import Path
-from typing import Any
 
 import typer
 from typing_extensions import Annotated
-
-from .runner.data_sink.local_file_sink_with_duck import LocalFileSinkWithDuck
-from .runner.data_source.base import DataSource
-from .runner.data_source.foundry_source_with_duck import FoundrySourceWithDuck
-from .runner.data_source.local_file_source import LocalDataSource
-from .runner.data_source.mixed_source import MixedDataSource
 
 
 class Engine(str, Enum):
@@ -21,15 +12,6 @@ class Engine(str, Enum):
 
 
 if __name__ == "__main__":
-
-    def import_from_path(module_name: str, file_path: str):
-        spec = importlib.util.spec_from_file_location(module_name, file_path)
-        module = importlib.util.module_from_spec(spec)  # type: ignore
-        sys.modules[module_name] = module  # Register the module in sys.modules
-        spec.loader.exec_module(  # type:ignore
-            module
-        )  # Execute the module in its own namespace
-        return module
 
     def traverse_to_setup_and_add_to_path(module_name: str) -> None:
         parent = Path(module_name).parent
@@ -57,6 +39,7 @@ if __name__ == "__main__":
             str, typer.Option(help="Branch name for local development")
         ] = "duck-fndry-dev",
     ):
+        traverse_to_setup_and_add_to_path(transform_to_run)
         if engine == "duckdb":
             from transforms.engine.duckdb import init_sess
 
@@ -65,53 +48,15 @@ if __name__ == "__main__":
             from transforms.engine.spark import init_sess
 
             session = init_sess()
+        from .runner.default_executor import execute_with_default_foundry
 
-        from foundry_dev_tools import FoundryContext
-
-        from transforms.api.transform_df import Transform
-        from transforms.runner.exec_transform import TransformRunner
-        traverse_to_setup_and_add_to_path(transform_to_run)
-        mod = import_from_path("transform", transform_to_run)
-        transforms: dict[str, Transform | Any] = {}
-        for name, item in mod.__dict__.items():
-            if isinstance(item, Transform):
-                transforms[name] = item
-        if len(transforms) > 1:
-            print("There is more than one transform specified. Please specify its name")
-            print("names are", list(transforms.keys()))
-            return
-        if len(transforms) == 0:
-            print("file has no transforms")
-            return
-
-        branches = fallback_branches.split(",")
-        all_branches = [local_dev_branch_name] + branches
-        fndry_ctx = FoundryContext()
-
-        def get_dataset_name(dataset_path_or_rid: str) -> str:
-            dataset_path = str(fndry_ctx.get_dataset(dataset_path_or_rid).path)
-            sha_addon = sha256(dataset_path.encode()).hexdigest()[:2]
-            dataset_name = dataset_path.split("/")[-1]
-            return f"{dataset_name}_{sha_addon}"
-
-        foundry_source = FoundrySourceWithDuck(ctx=FoundryContext(), session=session, get_dataset_dataset_name=get_dataset_name)
-        local_source = LocalDataSource(session=session)
-        sources_mapping: dict[str, DataSource] = {b: foundry_source for b in branches}
-        sources_mapping[local_dev_branch_name] = local_source
-
-        TransformRunner(
-            sink=LocalFileSinkWithDuck(
-                branch=local_dev_branch_name, get_dataset_dataset_name=get_dataset_name
-            ),
-            sourcer=MixedDataSource(
-                sources=sources_mapping,
-                fallback_source=foundry_source,
-            ),
-            fallback_branches=all_branches,
-        ).exec_transform(
-            list(transforms.values())[0],
-            omit_checks=omit_checks,
+        execute_with_default_foundry(
             dry_run=dry_run,
+            fallback_branches=fallback_branches,
+            session=session,
+            local_dev_branch_name=local_dev_branch_name,
+            omit_checks=omit_checks,
+            transform_to_run=transform_to_run
         )
 
     typer.run(main)
