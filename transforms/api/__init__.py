dd56cec580168e757630baf1290f12fef6b3792f
Emil Redzik <emilredzik1994@gmail.com>
2024-11-05 14:02:07 +0100

    added exports to api

--- transforms/api/__init__.py
+++ transforms/api/__init__.py
@@ -1,7 +1,15 @@
 from .check import Check
 from .configure import configure
 from .incremental_transform import incremental
-from .transform_df import Input, Output, Transform, transform, transform_df
+from .transform_df import (
+    Input,
+    Output,
+    Transform,
+    TransformInput,
+    TransformOutput,
+    transform,
+    transform_df,
+)
 
 __all__ = [
     "Input",
@@ -12,4 +20,6 @@ __all__ = [
     "configure",
     "incremental",
     "transform",
+    "TransformInput",
+    "TransformOutput",
 ]
