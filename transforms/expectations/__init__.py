f121a44e3591028805a6bcc816c4f50505524e40
Emil Redzik <emilredzik1994@gmail.com>
2024-11-07 16:08:36 +0100

    added groupby expectations

--- transforms/expectations/__init__.py
+++ transforms/expectations/__init__.py
@@ -1,5 +1,6 @@
 from .colexpects import all, any, col, primary_key, schema
 from .count_expectations import count
+from .grouped import group_by
 
 __all__ = [
     "count",
@@ -7,5 +8,6 @@ __all__ = [
     "primary_key",
     "col",
     "any",
-    "all"
+    "all",
+    "group_by",
 ]
