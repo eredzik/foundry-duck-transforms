db4778a7fdc086e3f61f95df493cb0d40816eba4
Emil Redzik <emilredzik1994@gmail.com>
2026-06-15 15:05:07 +0200

    Remove unused TypeAlias definitions in `transforms/types.py` to streamline code and improve readability.

--- transforms/types.py
+++ transforms/types.py
@@ -1,16 +1,3 @@
 from typing import TypeAlias, Literal, TYPE_CHECKING
 if TYPE_CHECKING:
     from pyspark.sql import DataFrame
-
-
-    ds_ds_a:TypeAlias = 'DataFrame[Literal["v"]]'
-
-    ds_ds_b:TypeAlias = 'DataFrame[Literal["v"]]'
-
-    ds_ds_c:TypeAlias = 'DataFrame[Literal["v"]]'
-
-    name_ds_a:TypeAlias = 'DataFrame[Literal[]]'
-
-    name_ds_b:TypeAlias = 'DataFrame[Literal[]]'
-
-    name_ds_c:TypeAlias = 'DataFrame[Literal[]]'
